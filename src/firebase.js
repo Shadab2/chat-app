88dd3cf351fe376ad03ddb04e94dd1c6be76f07a
Shaad <ssidique610@gmail.com>
2022-01-26 16:00:30 +0530

    firebase added to thre project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCBGz6RrtJ9IO4c2yplaIHddEvOfGsczDg',
+  authDomain: 'chat-web-app-e600c.firebaseapp.com',
+  projectId: 'chat-web-app-e600c',
+  storageBucket: 'chat-web-app-e600c.appspot.com',
+  messagingSenderId: '932342104365',
+  appId: '1:932342104365:web:1784586d0b0c7f876a53a4',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
