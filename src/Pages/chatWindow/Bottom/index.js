40b8f8ce2e39bf2f869494032c28659051ffd389
Shaad <ssidique610@gmail.com>
2022-02-08 15:25:00 +0530

    Added last messagge functionality with the feature to save messages to the databse

--- src/Pages/chatWindow/Bottom/index.js
+++ src/Pages/chatWindow/Bottom/index.js
@@ -1,7 +1,73 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState, useCallback } from 'react';
+import firebase from 'firebase/app';
+import { database } from '../../../firebase';
+import { useParams } from 'react-router';
+import { InputGroup, Icon, Input, Alert } from 'rsuite';
+import { useProfile } from '../../../context/ProfileContext';
+
+function formatMessage(chatId, profile, text) {
+  return {
+    roomId: chatId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    text,
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
 
 function index() {
-  return <div>Bottom</div>;
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { profile } = useProfile();
+  const { chatId } = useParams();
+
+  const onHandleChange = useCallback(value => {
+    setMessage(value);
+  }, []);
+
+  const onSendClick = async () => {
+    if (message.trim() === '') return;
+    const formatedMessage = formatMessage(chatId, profile, message);
+
+    const updates = {};
+    const messageId = database.ref('messages').push().key;
+    updates[`/messages/${messageId}`] = formatedMessage;
+    updates[`/rooms/${chatId}/lastMessage`] = {
+      ...formatedMessage,
+      msgId: messageId,
+    };
+    setLoading(true);
+    try {
+      await database.ref().update(updates);
+      setMessage('');
+      setLoading(false);
+    } catch (e) {
+      Alert.error(e, 2000);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <InputGroup>
+      <Input
+        placeholder="Write your message here..."
+        onChange={onHandleChange}
+      />
+      <InputGroup.Button
+        appearance="primary"
+        color="blue"
+        onClick={onSendClick}
+        disabled={loading}
+      >
+        <Icon icon="send" />
+      </InputGroup.Button>
+    </InputGroup>
+  );
 }
 
 export default index;
--- src/Pages/chatWindow/Top/index.js
+++ src/Pages/chatWindow/Top/index.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { Icon } from 'rsuite';
+import { Link } from 'react-router-dom';
 import { useCurrentRoom } from '../../../context/CurrentRoomContext';
+import { useMediaQuery } from '../../../customHooks/customHooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
-function index() {
+function Top() {
   const name = useCurrentRoom(v => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width: 992px)');
+  console.log(isMobile);
+
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4 className="text-disappear d-flex align-items-center">
+          <div>
+            <div
+              className={
+                isMobile ? 'd-inline-block p-0 mr-2 text-blue' : 'd-none'
+              }
+            >
+              <Icon
+                componentClass={Link}
+                to="/"
+                icon="arrow-circle-left"
+                size="2x"
+              />
+            </div>
+          </div>
+          <span className="text-disappear">{name}</span>
+        </h4>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 }
 
-export default index;
+export default memo(Top);
